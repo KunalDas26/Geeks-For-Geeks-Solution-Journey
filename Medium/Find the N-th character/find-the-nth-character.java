a2f7bc458bbb8bd0e6f0268b89d985edda453c23
KUNAL DAS <143780662+KunalDas26@users.noreply.github.com>
2024-03-09 19:57:44 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find the N-th character/find-the-nth-character.java
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String S = sc.next();
+                    int R = sc.nextInt();
+                    int N = sc.nextInt();
+                    Solution obj = new Solution();
+                    System.out.println(obj.nthCharacter(S,R,N));
+                }
+                
+        }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public char nthCharacter(String s, int r, int n)
+    {
+        if(r==0) 
+            return s.charAt(n);
+        char value=nthCharacter(s,r-1,n/2);
+        String curr=value=='0'?"01":"10";
+        return n%2==0?curr.charAt(0):curr.charAt(1);
+    }
+}
